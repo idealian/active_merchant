0f209ed619faeafdae6abb855279b02d9eb33c81
speedmax <subjective@gmail.com>
2010-08-02 17:19:52 +1000

    Add recuring payment support for SecurePay AU gateway.
     - refactor securepay gateway to support both payment/periodic
     - implement SecurePayAuGateway#recurring and SecurePayAuGateway#cancel_recurring methods and update remote gateway test

--- lib/active_merchant/billing/gateways/secure_pay_au.rb
+++ lib/active_merchant/billing/gateways/secure_pay_au.rb
@@ -5,8 +5,8 @@ module ActiveMerchant #:nodoc:
     class SecurePayAuGateway < Gateway
       API_VERSION = 'xml-4.2'
       
-      TEST_URL = 'https://www.securepay.com.au/test/payment'
-      LIVE_URL = 'https://www.securepay.com.au/xmlapi/payment'
+      TEST_URL = 'https://test.securepay.com.au/xmlapi'
+      LIVE_URL = 'https://www.securepay.com.au/xmlapi'
       
       self.supported_countries = ['AU']
       self.supported_cardtypes = [:visa, :master, :american_express, :diners_club, :jcb]
@@ -33,11 +33,24 @@ module ActiveMerchant #:nodoc:
         :authorization => 10,
         :capture => 11,
         :void => 6,
-        :credit => 4
+        :credit => 4,
+        :recurring => 14
       }
-      
+
       SUCCESS_CODES = [ '00', '08', '11', '16', '77' ]
       
+      # Recuring payments
+      RECURRING_ACTIONS = ['add', 'delete', 'trigger']
+      
+      RECURRING_INTERVALS = {
+        :weekly => 1,
+        :biweekly => 2,
+        :monthly => 3,
+        :quarterly => 4,
+        :halfyearly => 5,
+        :yearly => 6
+      }
+
       def initialize(options = {})
         requires!(options, :login, :password)
         @options = options
@@ -49,28 +62,86 @@ module ActiveMerchant #:nodoc:
       end
       
       def purchase(money, credit_card, options = {})
-        commit :purchase, build_purchase_request(money, credit_card, options)
+        commit :purchase, build_purchase_request(:purchase, money, credit_card, options)
       end                       
+
+      # Create a recuring payment
+      # Sees:  http://www.securepay.com.au/resources/Secure-XML-API/Integration-Guide-Periodic-and-Triggered-add-in-pg01.html
+      # * <tt>starting_at</tt> - Start date of the recurring payment
+      # * <tt>profile_id</tt> - Client reference id
+      # * <tt>name</tt> - The name of the customer to be billed.  If not specified, the name from the credit card is used.
+      # * <tt>periodicity</tt> - The frequency that the recurring payments will occur at.  Can be one of
+      # :weekly, :biweekly, :monthly, :semimonthly, :quarterly, :semiyearly, :yearly
+      # * <tt>payments</tt> - The term, or number of payments that will be made
+      def recurring(money, credit_card, options = {})
+        requires!(options, [:periodicity, :monthly, :weekly, :daily], :payments, :profile_id)
+        
+        request = build_recurring_request('add', money, credit_card, options)
+        commit :recurring, request
+      end
     
+      def cancel_recurring(profile_id, options = {})
+        requires!(options, :profile_id)
+
+        options[:profile_id] = profile_id
+        request = build_recurring_request('delete', nil, nil, options)
+        commit :recurring, request
+      end
+      
       private
       
-      def build_purchase_request(money, credit_card, options)
+      def build_recurring_request(action, money, credit_card, options)
+        unless RECURRING_ACTIONS.include?(action)
+          raise StandardError, "Invalid Recurring Profile Action: #{action}"
+        end
+        
+        periodicity = options[:periodicity]
         xml = Builder::XmlMarkup.new
         
-        xml.tag! 'amount', amount(money)
-        xml.tag! 'currency', options[:currency] || currency(money)              
-        xml.tag! 'purchaseOrderNo', options[:order_id].to_s.gsub(/[ ']/, '')
-        
-        xml.tag! 'CreditCardInfo' do
-          xml.tag! 'cardNumber', credit_card.number
-          xml.tag! 'expiryDate', expdate(credit_card)
-          xml.tag! 'cvv', credit_card.verification_value if credit_card.verification_value?
+        xml.tag! 'RequestType', 'Periodic'
+        xml.tag! 'Periodic' do
+          xml.tag! 'PeriodicList', "count" => 1 do
+            xml.tag! 'PeriodicItem', "ID" => 1 do
+              xml.tag! 'actionType', action
+              xml.tag! 'clientID', options[:profile_id]
+              
+              if action == 'add'
+                xml.tag! 'amount', amount(money)
+                xml.tag! 'startDate', (options[:starting_at] || Date.today).strftime('%Y%m%d')
+                xml.tag! 'periodicType', 3
+                xml.tag! 'paymentInterval', RECURRING_INTERVALS[periodicity]
+                xml.tag! 'numberOfPayments', options[:payments]
+                add_credit_card(xml, credit_card)
+              end
+            end
+          end
+        end
+      end
+      
+      def build_purchase_request(action, money, credit_card, options)
+        xml = Builder::XmlMarkup.new
+
+        xml.tag! 'RequestType', 'Payment'
+        xml.tag! 'Payment' do
+          xml.tag! 'TxnList', "count" => 1 do
+            xml.tag! 'Txn', "ID" => 1 do
+              xml.tag! 'txnType', TRANSACTIONS[action]
+              xml.tag! 'txnSource', 23
+              
+              xml.tag! 'amount', amount(money)
+              xml.tag! 'currency', options[:currency] || currency(money)              
+              xml.tag! 'purchaseOrderNo', options[:order_id].to_s.gsub(/[ ']/, '')
+
+              add_credit_card(xml, credit_card)
+            end
+          end
         end
         
         xml.target!
       end
       
       def build_request(action, body)
+        
         xml = Builder::XmlMarkup.new
         xml.instruct!
         xml.tag! 'SecurePayMessage' do
@@ -78,36 +149,43 @@ module ActiveMerchant #:nodoc:
             xml.tag! 'messageID', Utils.generate_unique_id.slice(0, 30)
             xml.tag! 'messageTimestamp', generate_timestamp
             xml.tag! 'timeoutValue', request_timeout
-            xml.tag! 'apiVersion', API_VERSION
+            xml.tag! 'apiVersion', action == :recurring ? "sp#{API_VERSION}" : API_VERSION
           end
           
           xml.tag! 'MerchantInfo' do
             xml.tag! 'merchantID', @options[:login]
             xml.tag! 'password', @options[:password]
           end
-          
-          xml.tag! 'RequestType', 'Payment'
-          xml.tag! 'Payment' do
-            xml.tag! 'TxnList', "count" => 1 do
-              xml.tag! 'Txn', "ID" => 1 do
-                xml.tag! 'txnType', TRANSACTIONS[action]
-                xml.tag! 'txnSource', 23
-                xml << body
-              end
-            end
-          end
+          xml << body
         end
         
         xml.target!
       end
-         
+      
+      def add_credit_card(xml, credit_card)
+        xml.tag! 'CreditCardInfo' do
+          xml.tag! 'cardNumber', credit_card.number
+          xml.tag! 'expiryDate', expdate(credit_card)
+          xml.tag! 'cvv', credit_card.verification_value if credit_card.verification_value?
+        end
+      end
+
       def commit(action, request)
-        response = parse(ssl_post(test? ? TEST_URL : LIVE_URL, build_request(action, request)))
+        response = parse(ssl_post(url_for(action), build_request(action, request)))
+        require 'pp'
+        pp build_request(action, request)
         
         Response.new(success?(response), message_from(response), response, 
           :test => test?, 
           :authorization => authorization_from(response)
         )
+      end
+      
+      def url_for(action)
+        url = test? ? TEST_URL : LIVE_URL
+        path = (action == :recurring) ? '/periodic' : '/payment'
+        
+        url + path
       end
       
       def success?(response)
--- test/remote/gateways/remote_secure_pay_au_test.rb
+++ test/remote/gateways/remote_secure_pay_au_test.rb
@@ -14,20 +14,20 @@ class RemoteSecurePayAuTest < Test::Unit::TestCase
       :description => 'Store Purchase'
     }
   end
-  
+
   def test_successful_purchase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
     assert_equal 'Approved', response.message
   end
-
+  
   def test_unsuccessful_purchase
     @credit_card.year = '2005'
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
     assert_equal 'CARD EXPIRED', response.message
   end
-
+  
   def test_invalid_login
     gateway = SecurePayAuGateway.new(
                 :login => '',
@@ -37,4 +37,34 @@ class RemoteSecurePayAuTest < Test::Unit::TestCase
     assert_failure response
     assert_equal "Invalid merchant ID", response.message
   end
+  
+  def test_successful_create_recurring_payment
+    client_id = generate_unique_id[0...10]
+
+    response = @gateway.recurring(999, @credit_card, 
+      :order_id => '1',
+      :billing_address => address,
+      :description => 'Store Purchase',
+      :profile_id => client_id,
+      :periodicity => :monthly,
+      :payments => 20
+    )
+    
+    assert_success response
+  end
+  
+  def test_successful_cancel_recurring_payment
+    client_id = generate_unique_id[0...10]
+    
+    @gateway.recurring(123456, @credit_card, 
+      :profile_id => client_id,
+      :periodicity => :monthly,
+      :payments => 20
+    )
+    
+    response = @gateway.cancel_recurring(client_id)
+    
+    assert_success response
+  end
+
 end
